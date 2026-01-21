c1e412ca41ae8240eca0de6c1873d7f9b6b1ddc0
kfakhrik <kfakhrk@gmail.com>
2026-01-21 10:49:21 +0700

    update query

--- QUERYBUILDER.sql
+++ QUERYBUILDER.sql
@@ -13,3 +13,28 @@ CREATE TABLE sessionadmin (
 )
 
 INSERT INTO `session` (`id`, `username`, `password`) VALUES (NULL, 'Tomlut', MD5('Pizza123'));
+
+
+/* TABLE ANGGOTA */
+
+class Anggota extends Database;
+
+CREATE TABLE anggota (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nama VARCHAR(100) NOT NULL,
+    npm VARCHAR(20) NOT NULL,
+    quotes TEXT,
+    foto VARCHAR(255)
+);
+
+INSERT INTO anggota (nama, npm, quotes, foto) VALUES ('nama', 'npm', 'quotes', 'foto');
+
+UPDATE anggota
+SET 
+    nama = 'nama',
+    npm = 'npm',
+    quotes = 'quotes',
+    foto = 'file'
+WHERE id = 1;
+
+DELETE FROM anggota WHERE id = 1;
